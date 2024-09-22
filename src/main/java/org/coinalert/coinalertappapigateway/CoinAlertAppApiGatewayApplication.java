baa83f11986a8131dfa3f0964d5ffdbb5b86d997
baekhun <baekhun2463@gmail.com>
2024-09-22 22:11:30 +0900

    init

---
+++ src/main/java/org/coinalert/coinalertappapigateway/CoinAlertAppApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.coinalert.coinalertappapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinAlertAppApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoinAlertAppApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/coinalert/coinalertappapigateway/CoinAlertAppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coinalert.coinalertappapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinAlertAppApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
